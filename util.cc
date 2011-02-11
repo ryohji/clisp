bbdf66a1636cc163cca3011695821436e1117c63
Ryohji IKEBE <ryohji.ikebe@gmail.com>
2011-02-11 14:50:06 +0900

    separete sp in header file

--- util.cc
+++ util.cc
@@ -1,39 +1,5 @@
+#include "sp.h"
 #include <iostream>
-
-template <typename TYPE>
-struct sp {
-  explicit sp(TYPE *o = 0) : c(new unsigned(1)), o(o) {}
-  sp(const sp<TYPE>& u) : c(u.c), o(u.o) { ++*c; }
-  ~sp() { detach(); }
-  sp<TYPE>& operator=(const sp<TYPE>& u);
-  const TYPE* operator->() const { return o; }
-  TYPE* operator->();
-private:
-  void detach();
-  unsigned *c;
-  TYPE *o;
-};
-
-template<typename TYPE>
-inline sp<TYPE>& sp<TYPE>::operator=(const sp<TYPE>& u) {
-  ++*u.c;
-  detach();
-  o = u.o;
-  c = u.c;
-}
-
-template<typename TYPE>
-inline void sp<TYPE>::detach() {
-  if (0 == --*c) {
-    delete c;
-    delete o;
-  }
-}
-
-template<typename TYPE>
-inline TYPE* sp<TYPE>::operator->() {
-  return const_cast<TYPE*>(static_cast<const sp<TYPE>&>(*this).operator->());
-}
 
 struct object {
   object() { std::cout << "hello! (" << this << ")" << std::endl; }
