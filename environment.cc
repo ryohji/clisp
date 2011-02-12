a3f26808b77c49c4ddd2f71e92e64cc3b99712bd
Ryohji IKEBE <ryohji.ikebe@gmail.com>
2011-02-12 13:29:14 +0900

    implement environment

--- environment.cc
+++ environment.cc
@@ -1,8 +1,46 @@
 #include "environment.h"
 
 #include <list>
+#include <stdexcept>
+#include <utility>
 
 NS_CLISP_BEGIN
+
+typedef std::pair<expression_t, expression_t> pair_t;
+
+struct environment::impl : std::list<pair_t> {};
+
+namespace {
+  struct finder {
+    explicit finder(const expression_t expr) : symbol(expr->str()) {}
+    bool operator() (const pair_t &pair) const {
+      return symbol == pair.first->str();
+    }
+  private:
+    finder& operator= (const finder&);
+    const std::string symbol;
+  };
+}
+
+
+environment::environment() : o(new impl) {}
+
+environment::environment(const environment &env) : o (new impl(*env.o)) {}
+
+environment::~environment() { delete o; }
+
+expression_t environment::find(const expression_t &expr) const
+{
+  const impl::const_iterator it = std::find_if(o->begin(), o->end(), finder(expr));
+  if (o->end() == it)
+    throw std::runtime_error("symbol not found in the environment");
+  return it->second;
+}
+
+void environment::add(const expression_t &symbol, const expression_t &expr)
+{
+  o->push_front(std::make_pair(symbol, expr));
+}
 
 NS_CLISP_END
 
--- environment.h
+++ environment.h
@@ -8,14 +8,19 @@ NS_CLISP_BEGIN
 struct environment {
   environment();
 
+  environment(const environment& env);
+
   ~environment();
 
-  /* find matching expression to expr as symbol. if not found, throw runtime_exception. */
+  /* find matching expression to expr as symbol. if not found, throw runtime_error. */
   expression_t find(const expression_t &expr) const;
 
   void add(const expression_t &symbol, const expression_t &expr);
 
 private:
+  /* disable assignment */
+  environment& operator= (const environment &);
+
   struct impl;
   impl *o;
 };
