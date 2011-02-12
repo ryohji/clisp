fb2b71ec122d8c63a903d28b2051136ad2ebe742
Ryohji IKEBE <ryohji.ikebe@gmail.com>
2011-02-12 16:08:16 +0900

    add test usage of envromnet

--- clisp.cc
+++ clisp.cc
@@ -1,6 +1,8 @@
 #include "expression.h"
 #include "number.h"
 #include "application.h"
+#include "environment.h"
+#include "symbol.h"
 
 #include <iostream>
 
@@ -10,6 +12,12 @@ int main() {
   es.push_back(new clisp::number(1.0));
   es.push_back(new clisp::number(2.0));
   clisp::expression_t e = clisp::application::add().apply(es);
+  std::cout << e->str() << std::endl;
+
+  clisp::environment_t env(new clisp::environment);
+  env->add(new clisp::symbol("+"), new clisp::application::add);
+  env->add(new clisp::symbol("pi"), new clisp::number(3.14));
+  e = env->find("pi");
   std::cout << e->str() << std::endl;
   return 0;
 }
